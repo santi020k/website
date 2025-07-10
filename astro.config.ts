cb590407df28a4108545aba27fd8ac1817e06269
Santiago Molina Orozco <santi020k@gmail.com>
2025-07-10 13:50:23 -0500

    Fix astro config eslint errors

--- astro.config.ts
+++ astro.config.ts
@@ -1,38 +1,37 @@
-import fs from "node:fs";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
-import robotsTxt from "astro-robots-txt";
-import webmanifest from "astro-webmanifest";
-import { defineConfig, envField } from "astro/config";
-import { siteConfig } from "./src/site.config";
-
-// Remark plugins
-import remarkDirective from "remark-directive"; /* handle ::: directives as nodes */
-import { remarkAdmonitions } from "./src/plugins/remark-admonitions"; /* add admonitions */
-import { remarkReadingTime } from "./src/plugins/remark-reading-time";
-
+import { defineConfig, envField } from 'astro/config'
+import icon from 'astro-icon'
+import robotsTxt from 'astro-robots-txt'
+import webmanifest from 'astro-webmanifest'
+import fs from 'node:fs'
 // Rehype plugins
-import rehypeExternalLinks from "rehype-external-links";
-import rehypeUnwrapImages from "rehype-unwrap-images";
+import rehypeExternalLinks from 'rehype-external-links'
+import rehypePrettyCode from 'rehype-pretty-code'
+import rehypeUnwrapImages from 'rehype-unwrap-images'
+// Remark plugins
+import remarkDirective from 'remark-directive' /* handle ::: directives as nodes */
 
-import rehypePrettyCode from "rehype-pretty-code";
+import { remarkAdmonitions } from './src/plugins/remark-admonitions' /* add admonitions */
+import { remarkReadingTime } from './src/plugins/remark-reading-time'
+import { siteConfig } from './src/site.config'
+
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 import {
   transformerMetaHighlight,
-  transformerNotationDiff,
-} from "@shikijs/transformers";
+  transformerNotationDiff
+} from '@shikijs/transformers'
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["webmention.io"],
+    domains: ['webmention.io']
   },
   integrations: [
     icon(),
     tailwind({
       applyBaseStyles: false,
-      nesting: true,
+      nesting: true
     }),
     sitemap(),
     mdx(),
@@ -43,40 +42,41 @@ export default defineConfig({
        * required
        **/
       name: siteConfig.title,
+
       /**
        * optional
        **/
       // short_name: "Astro_Citrus",
       description: siteConfig.description,
       lang: siteConfig.lang,
-      icon: "public/icon.svg", // the source for generating favicon & icons
+      icon: 'public/icon.svg', // the source for generating favicon & icons
       icons: [
         {
-          src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
-          sizes: "180x180",
-          type: "image/png",
+          src: 'icons/apple-touch-icon.png', // used in src/components/BaseHead.astro L:26
+          sizes: '180x180',
+          type: 'image/png'
         },
         {
-          src: "icons/icon-192.png",
-          sizes: "192x192",
-          type: "image/png",
+          src: 'icons/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png'
         },
         {
-          src: "icons/icon-512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
+          src: 'icons/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
       ],
-      start_url: "/",
-      background_color: "#1d1f21",
-      theme_color: "#2bbc8a",
-      display: "standalone",
+      start_url: '/',
+      background_color: '#1d1f21',
+      theme_color: '#2bbc8a',
+      display: 'standalone',
       config: {
         insertFaviconLinks: false,
         insertThemeColorMeta: false,
-        insertManifestLink: false,
-      },
-    }),
+        insertManifestLink: false
+      }
+    })
   ],
   markdown: {
     syntaxHighlight: false,
@@ -84,84 +84,84 @@ export default defineConfig({
     remarkPlugins: [remarkReadingTime, remarkDirective, remarkAdmonitions],
     remarkRehype: {
       footnoteLabelProperties: {
-        className: [""],
+        className: ['']
       },
-      footnoteBackContent: "⤴",
+      footnoteBackContent: '⤴'
     },
 
     rehypePlugins: [
       [
         rehypeExternalLinks,
         {
-          rel: ["nofollow", "noreferrer"],
-          target: "_blank",
-        },
+          rel: ['nofollow', 'noreferrer'],
+          target: '_blank'
+        }
       ],
 
       [
         rehypePrettyCode,
         {
           theme: {
-            light: "rose-pine-dawn", // after changing the theme, the server needs to be restarted
-            dark: "rose-pine", // after changing the theme, the server needs to be restarted
+            light: 'rose-pine-dawn', // after changing the theme, the server needs to be restarted
+            dark: 'rose-pine' // after changing the theme, the server needs to be restarted
           },
 
-          transformers: [transformerNotationDiff(), transformerMetaHighlight()],
-        },
+          transformers: [transformerNotationDiff(), transformerMetaHighlight()]
+        }
       ],
-      rehypeUnwrapImages,
-    ],
+      rehypeUnwrapImages
+    ]
   },
   // https://docs.astro.build/en/guides/prefetch/
   prefetch: true,
-  // ! Please remember to replace the following site property with your own domain
-  site: "http://astrocitrus.artemkutsan.pp.ua/",
+  site: 'http://santi020k.me/',
   vite: {
     build: {
-      sourcemap: true, // Source maps generation
+      sourcemap: true // Source maps generation
     },
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
+      exclude: ['@resvg/resvg-js']
     },
-    plugins: [rawFonts([".ttf", ".woff"])],
+    plugins: [rawFonts(['.ttf', '.woff'])]
   },
   env: {
     schema: {
       WEBMENTION_API_KEY: envField.string({
-        context: "server",
-        access: "secret",
-        optional: true,
+        context: 'server',
+        access: 'secret',
+        optional: true
       }),
       WEBMENTION_URL: envField.string({
-        context: "client",
-        access: "public",
-        optional: true,
+        context: 'client',
+        access: 'public',
+        optional: true
       }),
       WEBMENTION_PINGBACK: envField.string({
-        context: "client",
-        access: "public",
-        optional: true,
-      }),
-    },
+        context: 'client',
+        access: 'public',
+        optional: true
+      })
+    }
   },
   server: {
     // port: 1234,
-    host: true,
-  },
-});
+    host: true
+  }
+})
 
 function rawFonts(ext: string[]) {
   return {
-    name: "vite-plugin-raw-fonts",
+    name: 'vite-plugin-raw-fonts',
     // @ts-expect-error:next-line
     transform(_, id) {
-      if (ext.some((e) => id.endsWith(e))) {
-        const buffer = fs.readFileSync(id);
+      if (ext.some(e => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id)
+
         return {
           code: `export default ${JSON.stringify(buffer)}`,
-          map: null,
-        };
+          map: null
+        }
       }
-    },
-  };
+    }
+  }
 }
