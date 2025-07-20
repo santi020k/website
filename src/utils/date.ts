fde5493067113b1d5dbfe025944181f0da9d906c
Santiago Molina <santi020k@gmail.com>
2025-07-20 15:49:31 -0500

    Fix Tailwind update

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,20 +3,26 @@ import type { CollectionEntry } from 'astro:content'
 import { siteConfig } from '@/site.config'
 
 export const getFormattedDate = (
-  date: Date | undefined,
-  options?: Intl.DateTimeFormatOptions
+  input: Date | string | number | undefined,
+  options: Intl.DateTimeFormatOptions = {},
 ): string => {
-  if (date === undefined) {
-    return 'Invalid Date'
-  }
+  // 1 → Valida que haya dato
+  if (input === undefined) return 'Invalid Date'
 
-  return new Intl.DateTimeFormat(siteConfig.date.locale, {
-    ...(siteConfig.date.options as Intl.DateTimeFormatOptions),
-    ...options
-  }).format(date)
+  // 2 → Garantiza un Date
+  const date = input instanceof Date ? input : new Date(input)
+
+  // 3 → Comprueba que el Date sea válido
+  if (Number.isNaN(date.getTime())) return 'Invalid Date'
+
+  // 4 → Formatea
+  return new Intl.DateTimeFormat(
+    siteConfig.date.locale,
+    { ...(siteConfig.date.options as Intl.DateTimeFormatOptions), ...options },
+  ).format(date)
 }
 
 export const collectionDateSort = (
   a: CollectionEntry<'post' | 'note'>,
   b: CollectionEntry<'post' | 'note'>
-) => b.data.publishDate.getTime() - a.data.publishDate.getTime()
+) => b?.data?.publishDate?.getTime?.() - a?.data?.publishDate?.getTime?.()
