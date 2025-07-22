947bcb2adddeda04288c9ea4344c50a4296cf1b8
= <=>
2025-07-22 14:00:22 -0500

    Change site config

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,10 +2,10 @@ import type { SiteConfig } from '@/types'
 
 export const siteConfig: SiteConfig = {
   // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-  author: 'Artem Kutsan',
+  author: 'Santi020k',
   // Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
   date: {
-    locale: 'en-GB',
+    locale: 'en-US',
     options: {
       day: 'numeric',
       month: 'short',
@@ -13,13 +13,13 @@ export const siteConfig: SiteConfig = {
     }
   },
   // Used as the default description meta property and webmanifest description
-  description: 'An opinionated starter theme for Astro',
+  description: 'The personal web site of Santiago Molina Orozco',
   // HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
-  lang: 'en-GB',
+  lang: 'en-US',
   // Meta property, found in src/components/BaseHead.astro L:42
-  ogLocale: 'en_GB',
+  ogLocale: 'en_US',
   // Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-  title: 'Citrus'
+  title: 'Santi020k'
 }
 
 // Used to generate links in both the Header & Footer.
@@ -28,9 +28,13 @@ export const menuLinks: { path: string, title: string }[] = [
     path: '/',
     title: 'Home'
   },
+  // {
+  //   path: '/about/',
+  //   title: 'About'
+  // },
   {
-    path: '/about/',
-    title: 'About'
+    path: '/portfolio/',
+    title: 'Portfolio'
   },
   {
     path: '/posts/',
@@ -39,5 +43,9 @@ export const menuLinks: { path: string, title: string }[] = [
   {
     path: '/notes/',
     title: 'Notes'
+  },
+  {
+    path: '/contact/',
+    title: 'Contact'
   }
 ]
