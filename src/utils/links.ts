10db4c7cd9b285d66fa1e5ffb383573060b7c9ef
Santiago Molina <santi020k@gmail.com>
2026-04-03 08:50:28 -0500

    refactor: rename projects route and path helpers to portfolio

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,5 +1,5 @@
 export const getPostPath = (slug: string) => `/blog/${slug}/`
 
-export const getProjectPath = (slug: string) => `/projects/${slug}/`
+export const getPortfolioPath = (slug: string) => `/portfolio/${slug}/`
 
 export const getTechnologyPath = (technology: string) => `/technologies/${encodeURIComponent(technology)}/`
